940e5ebbd4e7802d90ec379dcb3fd5b6beffadcc
Gift <aghaulor.gift@gmail.com>
2024-01-03 02:20:45 -0800

    Create 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        upper_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+        print("{}".format(upper_char), end="")
+    print()
+
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
+
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print("{}".format(last_digit), end="")
+    return last_digit
