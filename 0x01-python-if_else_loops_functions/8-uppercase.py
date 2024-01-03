a3db326cd599181d4829ed5b1caf7fe4deb832e5
Gift <aghaulor.gift@gmail.com>
2024-01-03 03:04:58 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def uppercase(s):
-    for char in s:
+def uppercase(str):
+    for char in str:
         upper_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
         print("{}".format(upper_char), end="")
     print()
@@ -9,9 +9,3 @@ def uppercase(s):
 uppercase("holberton")
 uppercase("Holberton School")
 uppercase("Holberton School 98 Battery street")
-
-
-def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print("{}".format(last_digit), end="")
-    return last_digit
