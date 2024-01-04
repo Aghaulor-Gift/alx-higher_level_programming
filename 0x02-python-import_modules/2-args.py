ee395f6ae3d659c61310d7ebad4ab21b53fd1a2a
Gift <aghaulor.gift@gmail.com>
2024-01-04 12:29:48 -0800

    Create 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     args_count = len(argv) - 1
 
-    print(args_count, "argument" + ("s" if args_count != 1 else "") + ":")
+    print("{:d} argument{}{}".format(args_count, "s" if args_count != 1 else "", ":" if args_count != 0 else "."))
 
     for i in range(1, len(argv)):
         print("{:d}: {}".format(i, argv[i]))
