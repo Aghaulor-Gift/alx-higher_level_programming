b412bfaccdaedf8316f6ac932dbdc1bb32fa9432
Gift <aghaulor.gift@gmail.com>
2024-01-06 00:27:41 -0800

    Create 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
+if __name__ == "__main__":
+
+    def print_list_integer(my_list=[]):
+        for _num in _my_list:
+            print("{:d}".format(num_))
