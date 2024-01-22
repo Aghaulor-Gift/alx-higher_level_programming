0c7c7456b73786b531fcc40935a16ee771b5792b
Gift <aghaulor.gift@gmail.com>
2024-01-22 14:12:28 -0800

    Create 3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+        if b != 0:
+            print("Inside result: {}".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
+    except ZeroDivisionError:
+        print("Inside result: {}".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
+    finally:
+        return result
