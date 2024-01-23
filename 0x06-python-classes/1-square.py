31c4200c39d7df3aca72d0112809faf360f319a9
Gift <aghaulor.gift@gmail.com>
2024-01-23 05:23:15 -0800

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        __size (int): Private instance attribute
+                        representing the size of the square.
+
+    Methods:
+        None
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square with a specified size.
+
+        Args:
+            size (int): Size of the square.
+        """
+        self.__size = size
