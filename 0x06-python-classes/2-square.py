7f5b57e034ffc472ffa0e2bd2118f6a29c25a4fa
Gift <aghaulor.gift@gmail.com>
2024-01-23 06:37:48 -0800

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        __size (int): Private instance attribute representing
+                        the size of the square.
+
+    Methods:
+        None
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a square with an optional specified size.
+
+        Args:
+            size (int, optional): Size of the square (default is 0).
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
