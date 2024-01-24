71ee6e7b59cd1ca158584035f35a4cf7f97b6fdd
Gift <aghaulor.gift@gmail.com>
2024-01-24 01:22:48 -0800

    Updated 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
+"""
+A class that defines a square.
+"""
 
 class Square:
     """
-    A class that defines a square.
-
-    Attributes:
-        __size (int): Private instance attribute representing
-                        the size of the square.
-
-    Methods:
-        None
+    Represent a square
     """
 
     def __init__(self, size=0):
