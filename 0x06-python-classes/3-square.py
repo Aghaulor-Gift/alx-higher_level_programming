c2af09a070cebfc56a8823ba622cb54e24d1c345
Gift <aghaulor.gift@gmail.com>
2024-01-24 01:35:29 -0800

    Review docstring in 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
+"""A class that defines a square."""
+
 
 class Square:
-    """
-    A class that defines a square.
-
-    Attributes:
-        __size (int): Private instance attribute representing
-                        the size of the square.
-
-    Methods:
-        area(): Public instance method that returns the current square area.
-    """
+    """Represents a square."""
 
     def __init__(self, size=0):
         """
