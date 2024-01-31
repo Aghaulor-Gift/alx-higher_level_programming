ecd7a351988387a0a31caf5b33447bd50b51431a
Gift <aghaulor.gift@gmail.com>
2024-01-31 14:17:11 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This is a Locked class module.
 Attributes:
-    LockedClass: A class that restricts dynamic creation of instance attributes.
+    LockedClass: A class that restricts dynamic creation\
+                of instance attributes.
 """
 
 
 class LockedClass:
     """Represent a LockedClass"""
+
     __slots__ = ('first_name',)
