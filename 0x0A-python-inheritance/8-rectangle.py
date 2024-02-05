7097bdc9caa70f03969f837c856dbd8825e75342
Gift <aghaulor.gift@gmail.com>
2024-02-05 14:40:40 -0800

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A module that defines a class
+"""
+
+
+class BaseGeometry:
+    """A class with area and integer_validator methods."""
+
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates the value"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """A class representing a rectangle."""
+
+    def __init__(self, width, height):
+    
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
