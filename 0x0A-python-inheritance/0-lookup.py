1cfc976a483c855c337701614910c839a356f832
Gift <aghaulor.gift@gmail.com>
2024-02-05 08:31:14 -0800

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Returns a list of available attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    Represent the lookup function that searches throught the list.
+    """
+    return [attr for attr in dir(obj) if not callable(getattr(obj, attr))
+            or not attr.startswith("__")]
