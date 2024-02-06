5857562b8cd15191cffa7567ea5bb961534e0054
Gift <aghaulor.gift@gmail.com>
2024-02-06 03:19:26 -0800

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Read File Module """
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, mode='r', encoding='utf-8') as file:
+        print(file.read(), end='')
+
+if __name__ == "__main__":
+    read_file()
