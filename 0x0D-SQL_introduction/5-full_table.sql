5636ad91554bce6bfb8c2503b8a8086295a756a6
Gift <aghaulor.gift@gmail.com>
2024-03-12 12:46:42 -0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- Script: 5-full_table.sql
 -- Description: Prints the full description of the table first_table
 
-SELECT *
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
