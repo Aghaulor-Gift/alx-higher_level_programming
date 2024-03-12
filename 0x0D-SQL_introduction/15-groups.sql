6294cbb08c5404ee2dd9480b6edc98168dda1a15
Gift <aghaulor.gift@gmail.com>
2024-03-12 13:33:51 -0700

    15-groups.sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- Script: 15-score_count.sql
+-- Description: Lists the number of records with the same score in the table second_table
+
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC, score DESC;
