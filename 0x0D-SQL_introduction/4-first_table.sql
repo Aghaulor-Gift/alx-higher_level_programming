4942ab083d551d2ed8d46471b4707d0cb13b511d
Gift <aghaulor.gift@gmail.com>
2024-03-12 12:30:34 -0700

    Create 4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script: 4-first_table.sql
+-- Description: Creates the table first_table in the current database
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
