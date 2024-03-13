7038e52686d7e3cccce8cd0694ee3b1349e9036c
Gift <aghaulor.gift@gmail.com>
2024-03-13 12:25:53 -0700

    Create 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Script: 2-create_read_user.sql
+-- Description:Query to create user_0d_2 if it does not exist
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
