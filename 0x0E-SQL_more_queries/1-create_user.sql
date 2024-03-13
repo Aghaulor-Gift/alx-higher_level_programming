be0c0f9dafca5d70063759577dbf4ff3191d7673
Gift <aghaulor.gift@gmail.com>
2024-03-13 12:15:47 -0700

    Create 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Script: 1-create_user.sql
+-- Description:Query to create user_0d if it does not exist
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
