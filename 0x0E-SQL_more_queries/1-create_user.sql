9c3fb8d2a3914d02bb432c5489ce141052bdd7d9
Gift <aghaulor.gift@gmail.com>
2024-03-13 12:21:09 -0700

    Create 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- Script: 1-create_user.sql
--- Description:Query to create user_0d if it does not exist
+-- Description: Query to create user_0d_1 if it does not exist
+-- Query to grant all privileges to user_0d_1
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
