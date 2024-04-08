90ec75d05d55598e69e1eaff7175f122e4e83fa6
Gift <aghaulor.gift@gmail.com>
2024-04-08 08:10:32 -0700

    Print arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argsNumber = process.argv.length;
+
+if (argsNumber === 2) {
+  console.log('No argument');
+} else if (argsNumber === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
