74ee7692d92771a66b3df1572517d8ba4219ca1a
Gift <aghaulor.gift@gmail.com>
2024-04-08 15:30:35 -0700

    Create x square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,11 @@ const number = parseInt(arg);
 
 if (!isNaN(number)) {
   for (let i = 0; i < number; i++) {
-    console.log('x'.repeat(number));
+    let row = '';
+    for (let j = 0; j < number; j++) {
+      row += 'X';
+    }
+    console.log(row);
   }
 } else {
   console.log('Missing size');
