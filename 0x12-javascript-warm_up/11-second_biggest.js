582d0b39704b3ada55ac78469c062411620baac1
Gift <aghaulor.gift@gmail.com>
2024-04-09 04:46:00 -0700

    Print second largestarg

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,6 @@ const argsCount = process.argv.length - 2;
 if (argsCount < 2) {
   console.log(0);
 } else {
-  
   const args = process.argv.slice(2).map(arg => parseInt(arg));
   args.sort((a, b) => b - a);
   console.log(args[1]);
