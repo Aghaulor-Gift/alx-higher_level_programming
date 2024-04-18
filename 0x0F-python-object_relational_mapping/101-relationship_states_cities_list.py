26e866991301b951f92da245a043380751d710d8
Gift <aghaulor.gift@gmail.com>
2024-04-18 04:26:16 -0700

    All states listed with their cities

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script to list all State objects and corresponding City objects from the database hbtn_0e_101_usa"""
+"""Script to list all State objects and corresponding
+City objects from the database hbtn_0e_101_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -19,7 +21,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
