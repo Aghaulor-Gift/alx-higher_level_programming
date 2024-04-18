7d2c919549f0e056a9626073757e27b989476012
Gift <aghaulor.gift@gmail.com>
2024-04-18 02:31:10 -0700

    Add class of city using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+    Contains class definition of a city and an instance
+    Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """Represents a city class of a state in the database"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer,  ForeignKey('state.id'), nullable=False)
