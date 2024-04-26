7a7059cfbd4754ac809f15901b03a4db413d20e8
Gift <aghaulor.gift@gmail.com>
2024-04-26 11:17:49 -0700

    Return error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    x = requests.get(url)
+    print(x.text)    
+
+    if (x.status_code >= 400):
+        print('Error code: ', x.status_code)
