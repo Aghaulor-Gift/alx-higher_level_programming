b60d4cf02ce20daaaa0453b1d81ea1f1c3d774b5
Gift <aghaulor.gift@gmail.com>
2024-04-26 11:21:32 -0700

    Return error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     x = requests.get(url)
-    print(x.text)    
+    print(x.text)
 
     if (x.status_code >= 400):
         print('Error code: ', x.status_code)
