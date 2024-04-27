74866c4855c392cba717965b104336d64b2200a0
Gift <aghaulor.gift@gmail.com>
2024-04-27 06:30:00 -0700

    Display content length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a request to that URL, and displays the size.
-curl -sI "$1" | grep -i 'content-length'| awk '{print "$2"}'
+curl -sI "$1" | grep -i 'content-length'| awk '{print $2}'
