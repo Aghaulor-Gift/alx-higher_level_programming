ed6014afb0e3028c3225ae56d2c03bc527a10119
Gift <aghaulor.gift@gmail.com>
2024-04-27 07:37:16 -0700

    Display body of status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes in a URL, sends a GET request to the URL, and displays the body
-curl -s -w "%{http_code}" -o /dev/null "$1" | grep -q '200' && curl -s "$1"
+curl -Ls "$1"
