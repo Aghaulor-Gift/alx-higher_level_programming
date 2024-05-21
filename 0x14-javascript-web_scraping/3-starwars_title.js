ace4ad5a765cadcacec92449d21558b5882b989f
Gift <aghaulor.gift@gmail.com>
2024-05-21 09:26:43 -0700

    Remove error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,7 +6,7 @@ const request = require('request');
 const movie_Id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/:movie_Id';
 
-request(url, (error, response, body) => {
+request(url, (err, response, body) => {
   if (err) {
     console.error(err);
     return;
