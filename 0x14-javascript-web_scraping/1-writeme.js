900305e46793c95863fd989787f0bc69f93d3845
Gift <aghaulor.gift@gmail.com>
2024-05-21 07:38:38 -0700

    Remove return statements

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,9 @@ const fs = require('fs');
 const filename = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filename, content, 'utf8', (err) => {
+fs.writeFile(filename, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log();
 }
 );
