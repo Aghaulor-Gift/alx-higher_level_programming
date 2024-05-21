ab0584df641f0ab662c314b003f4e41c470a0052
Gift <aghaulor.gift@gmail.com>
2024-05-21 09:22:08 -0700

    Print id title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,16 +3,16 @@
 // number matches a given integer.
 
 const request = require('request');
-const movie_Id = process.arrgv[2];
+const movie_Id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/:movie_Id';
-const film;
+
 request(url, (error, response, body) => {
   if (err) {
     console.error(err);
     return;
   }
   if (response.statusCode === 200) {
-    film = JSON.parse(body);
+    const film = JSON.parse(body);
     console.log(film.title);
   } else {
     console.log(`Error: ${response.statusCode}`);
