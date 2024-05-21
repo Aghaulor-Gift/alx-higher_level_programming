bde8918babad79122cfb9f9fa2dc1a45ce3f0858
Gift <aghaulor.gift@gmail.com>
2024-05-21 10:22:10 -0700

    Count with specific character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+// A  script that prints the number of movies where the character
+// “Wedge Antilles” is present.
+
+const request = require('request');
+const charID = '18';
+const apiUrl = process.argv[2];
+const charUrl = `https://swapi-api.alx-tools.com/api/people/${charID}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(film => {
+      if (film.characters.includes(charUrl)) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  } else {
+    console.log(`Error: ${response.statusCode}`);
+  }
+});
