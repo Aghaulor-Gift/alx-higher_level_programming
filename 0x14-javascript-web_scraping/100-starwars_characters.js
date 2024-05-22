80d5b32d921e2c2e6cedf75e283c251a3a26ee49
Gift <aghaulor.gift@gmail.com>
2024-05-22 05:07:57 -0700

    fix error

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,7 @@
 // A script that retrieves and prints the names of all Star Wars characters from the Star Wars API.
 
 const request = require('request');
-const movieId  = process.argv[2];
+const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(apiUrl, function (error, response, body) {
