49464f64b13572e9841dda04406cbc15fea41cbf
Gift <aghaulor.gift@gmail.com>
2024-05-29 14:27:03 -0700

    101-script.js

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-  $('#add_item').click(function() {
+$(document).ready(function () {
+  $('#add_item').click(function () {
     $('<li>Item</li>').appendTo('.my_list');
   });
 
-  $('#remove_item').click(function() {
+  $('#remove_item').click(function () {
     $('.my_list li:last-child').remove();
   });
 
-  $('#clear_list').click(function() {
+  $('#clear_list').click(function () {
     $('.my_list').empty();
   });
 });
